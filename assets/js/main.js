24fbdaa1dc5dd61f073dc0b227f30c15e0fe5ad1
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-15 12:10:52 +0200

    aggiunte strutture dati array

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+//lista ordinata di 5 immagini
+const items = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg'
+];
+
+
+//lista ordinata di 5 luoghi
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+//lista di 5 news
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+document.querySelector('ul.image-list').innerHTML = items;
